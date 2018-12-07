f839c1280d060a57251e9fd6b631aacd3f9d2b26
Martin Wimpress <code@flexion.org>
2018-12-07 15:08:05 +0000

    code: Add thread manager.

---
+++ code/threads.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+threads=()
+
+keep-thread() {
+  local pid=$!
+  threads+=("$pid")
+  echo $pid
+}
+
+kill-thread() {
+  local pid=$1
+  (kill -STOP "$pid" || true) > /dev/null 2>&1
+}
+
+terminate-all-threads() {
+  for index in "${!threads[@]}"; do
+    kill-thread "${threads["$index"]}"
+    unset threads["$index"]
+  done
+  threads=()
+}
