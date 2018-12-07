a3695cd3f57b1e60f189a9062c57deb53e60eef0
Martin Wimpress <code@flexion.org>
2018-12-07 15:11:09 +0000

    code: Add stub game logic.

---
+++ code/game.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+readonly STATUS_COLOR=7
+readonly NOTICE_COLOR=4
+readonly HERO='🐧'
+heroX=$(($(tput cols)/2))
+heroY=$(($(tput lines)/2))
+heroSize=${#HERO}
+
+reset-game() {
+  score=0
+  lives=3
+}
+
+game-mode() {
+  KEY='\0'
+  DELAY=0.05
+  tput clear
+  reset-game
+  music level$(shuf -i 1-3 -n 1)
+  gameMusicThread=$!
+  LOOP=game-loop
+}
+
+game-loop() {
+  oldheroX=$heroX
+  oldheroY=$heroY
+
+  erase "$heroX" "$heroY" "$heroSize"
+
+  # Movement
+  case $KEY in
+    'w')
+      ((heroY--));;
+    's')
+      ((heroY++));;
+    'a')
+      ((heroX--));;
+    'd')
+      ((heroX++));;
+    'v')
+      # Victory condition stub
+      kill-thread "$gameMusicThread"
+      victory-mode
+      return 1;;
+    'o')
+      # Game over condition stub
+      kill-thread "$gameMusicThread"
+      gameover-mode
+      return 1;;
+  esac
+  KEY='\0'
+
+  # Score and entities
+  draw $heroX $heroY 0 "$HERO"
+  draw 0 0 $STATUS_COLOR "Lives: $lives"
+  draw-right 0 $STATUS_COLOR "Score: $score"
+  
+  render
+
+  if [ $oldheroX -ne $heroX ] || [ $oldheroY -ne $heroY ]; then
+    sound move
+  fi
+}
