2c14e4ba1eda20401c76b5061ed3ac846457a190
Martin Wimpress <code@flexion.org>
2018-12-13 00:41:12 +0000

    gfx: Add colour to player 1 sprite. Add animated thrust.

--- gfx/sprites.sh
+++ gfx/sprites.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 
-readonly HERO=(
-"       "
-"   ▄   "
-"  ▄█▄  "
-" ▄███▄ "
-"▐█████▌"
-"  ▀ ▀  "
-"       ")
+export THRUST_FRAME=0
+export HERO_WIDTH=7
+export HERO_HEIGHT=7
+
+readonly THRUST=(
+"$DEF  $ylw▀$DEF $ylw▀  "
+"$DEF  $ylw▀$DEF $ylw▀  "
+"$DEF  $ylw▀$DEF $ylw▀  "
+"$DEF  $ylw$bred▓$DEF $ylw$bred▓$DEF  "
+"$DEF  $ylw$bred▓$DEF $ylw$bred▓$DEF  "
+"$DEF  $ylw$bred▓$DEF $ylw$bred▓$DEF  "
+"$DEF  $red$bylw▓$DEF $red$bylw▓$DEF  "
+"$DEF  $red$bylw▓$DEF $red$bylw▓$DEF  "
+"$DEF  $red$bylw▓$DEF $red$bylw▓$DEF  "
+)
+
+compose-sprites() {
+#       
+#   ▄   
+#  ▄█▄  
+# ▄███▄ 
+#▐█████▌
+#  ▀ ▀  
+#       
+HERO=(
+"$DEF       "
+"$DEF   $RED▄$DEF   "
+"$DEF  $blk▄$red█$blk▄  "
+"$DEF $RED▄$blk█$RED█$blk█$RED▄ "
+"$RED▐█$blk█$RED█$blk█$RED█▌"
+"${THRUST[$THRUST_FRAME]}"
+"$DEF       ")
+
+  [[ $THRUST_FRAME -ge 10 ]] && THRUST_FRAME=0 || ((THRUST_FRAME++))
+}
