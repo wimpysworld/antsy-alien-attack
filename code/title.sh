3fd245fdb0c1618397e6f2409205463c585fa897
Martin Wimpress <code@flexion.org>
2018-12-14 22:25:52 +0000

    code: Improve the control instructions.

--- code/title.sh
+++ code/title.sh
@@ -14,9 +14,12 @@ title-mode() {
 
   lol-draw-centered $((SCREEN_HEIGHT / 2 - 1)) "C O N T R O L S"
   lol-draw-centered $((SCREEN_HEIGHT / 2 + 1)) "W"
-  lol-draw-centered $((SCREEN_HEIGHT / 2 + 3)) "A     D"
-  lol-draw-centered $((SCREEN_HEIGHT / 2 + 5)) "S" 
-  lol-draw-centered $((SCREEN_HEIGHT / 2 + 7)) " Press [P] to Play or [Q] to Quit"
+  lol-draw-centered $((SCREEN_HEIGHT / 2 + 2)) "↑"
+  lol-draw-centered $((SCREEN_HEIGHT / 2 + 3)) "A ←   → D"
+  lol-draw-centered $((SCREEN_HEIGHT / 2 + 4)) "↓"
+  lol-draw-centered $((SCREEN_HEIGHT / 2 + 5)) "S"
+  lol-draw-centered $((SCREEN_HEIGHT / 2 + 7)) "[L] Unleash the lasers"
+  lol-draw-centered $((SCREEN_HEIGHT / 2 + 9)) " Press [P] to Play or [Q] to Quit"
 
   readarray -t titleScreen < gfx/title.ans
   titleScreenOffset=$(center 80)
