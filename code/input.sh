fbd43c65698d14d21fbe7602f6f079572f47f0bc
Martin Wimpress <code@flexion.org>
2018-12-16 22:48:18 +0000

    code: Add variable expansion.

--- code/input.sh
+++ code/input.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 export KEY=
-
-joystickThread=
-joy2key=$(command -v joy2key)
+export JOYSTICK_THREAD=
+JOY2KEY=$(command -v joy2key)
 
 joystick-setup() {
   # Is joy2key in the path and is there joystick connected
-  if [ -n "$joy2key" ] && [ -c /dev/input/js0 ]; then
-    $joy2key -rcfile cfg/xbox360.cfg > /dev/null 2>&1 &
-    joystickThread=$!
+  if [ -n "${JOY2KEY}" ] && [ -c /dev/input/js0 ]; then
+    ${JOY2KEY} -rcfile cfg/xbox360.cfg > /dev/null 2>&1 &
+    JOYSTICK_THREAD=$!
   fi
 }
 
 joystick-teardown() {
-  if [ -n "$joystickThread" ]; then
-    kill-thread $joystickThread
+  if [ -n "${JOYSTICK_THREAD}" ]; then
+    kill-thread ${JOYSTICK_THREAD}
   fi
 }
 
