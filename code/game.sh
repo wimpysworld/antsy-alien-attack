ce88de6582fad5c52c06b22aa3895e22561395b5
Martin Wimpress <code@flexion.org>
2018-12-16 21:24:15 +0000

    code: Make variable names consistent. Drop obsolete OldX/Y variables.

--- code/game.sh
+++ code/game.sh
@@ -3,14 +3,14 @@
 readonly STATUS_COLOR=$BRED$WHT
 
 reset-game() {
-  score=0
-  lives=3
+  P1_SCORE=0
+  P2_SCORE=0
+  P1_LIVES=3
+  P3_LIVES=3
   P1_X=$(( (SCREEN_WIDTH) /2 ))
   P1_Y=$(( (SCREEN_HEIGHT) /2 ))
-  P1_EndX=$(( SCREEN_WIDTH  - (PLAYER1_WIDTH + 2) ))
-  P1_EndY=$(( SCREEN_HEIGHT - PLAYER1_HEIGHT ))
-  P1_OldX=$P1_X
-  P1_OldY=$P1_Y
+  P1_MAX_X=$(( SCREEN_WIDTH  - (PLAYER1_WIDTH + 2) ))
+  P1_MAX_Y=$(( SCREEN_HEIGHT - PLAYER1_HEIGHT ))
   P1_LASERS=()
   P2_LASERS=()
   P1_LASER_CEILING=0
@@ -129,7 +129,7 @@ player-lasers() {
       erase-sprite 0 "$LASER_X" "$LASER_Y" "${P1_LASER_SPRITE[@]}"
       unset P1_LASERS[$LASER]
       P1_LASERS=("${P1_LASERS[@]}")
-      ((score++))
+      ((P1_SCORE++))
       ((IN_FLIGHT--))
       continue
     else
@@ -144,31 +144,27 @@ game-loop() {
   # Movement
   case $KEY in
     'w')
-      P1_OldY=$P1_Y
       ((P1_Y--))
       # Prevent leaving the top of the screen
       [ $P1_Y -lt 1 ] && P1_Y=1
       P1_LAST_KEY=$KEY
       ;;
     's')
-      P1_OldY=$P1_Y
       ((P1_Y++))
       # Prevent leaving the bottom of the screen
-      [ $P1_Y -gt $P1_EndY ] && P1_Y=$P1_EndY
+      [ $P1_Y -gt $P1_MAX_Y ] && P1_Y=$P1_MAX_Y
       P1_LAST_KEY=$KEY
       ;;
     'a')
-      P1_OldX=$P1_X
       ((P1_X--))
       # Prevent leaving screen left
       [ $P1_X -lt 0 ] && P1_X=0
       P1_LAST_KEY=$KEY
       ;;
     'd')
-      P1_OldX=$P1_X
       ((P1_X++))
       # Prevent leaving screne right
-      [ $P1_X -gt $P1_EndX ] && P1_X=$P1_EndX
+      [ $P1_X -gt $P1_MAX_X ] && P1_X=$P1_MAX_X
       P1_LAST_KEY=$KEY
       ;;
     'l')
@@ -201,8 +197,8 @@ game-loop() {
 
   compose-sprites
   animate-starfield
-  draw 0 0 "$STATUS_COLOR" "Lives: $lives"
-  draw-right 0 "$STATUS_COLOR" "Score: $score"
+  draw 0 0 "$STATUS_COLOR" "Lives: $P1_LIVES"
+  draw-right 0 "$STATUS_COLOR" "Score: $P1_SCORE"
   draw-sprite 1 "$P1_X" "$P1_Y" "${PLAYER1[@]}"
   fighter-ai
   player-lasers
