347bbaadf1b8c0ba580b336534f2898def874a93
Martin Wimpress <code@flexion.org>
2018-12-21 01:30:55 +0000

    code: Use arithmetic operators to determine SFX and Music configuration.

--- code/sfx.sh
+++ code/sfx.sh
@@ -6,7 +6,7 @@ sound-setup() {
   # Is this connection remote?
   if [ -n "${SSH_CLIENT}" ] || [ -n "${SSH_TTY}" ]; then
     SOUND_BACKEND='sound-silence'
-  elif [ ${SFX_ENABLED} -eq 0 ]; then
+  elif ((SFX_ENABLED == 0)); then
     SOUND_BACKEND='sound-silence'
   elif command -v mpg123 > /dev/null 2>&1; then
     SOUND_BACKEND='sound-mpg123'
