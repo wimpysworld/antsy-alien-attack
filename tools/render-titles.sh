fba46a8e6e67e2fc770ca89cd22dd4bda7d49721
Martin Wimpress <code@flexion.org>
2019-05-24 22:38:07 +0100

    tools: Update render-titles.sh
    Includes creation of round-up skills bonus screens.

--- tools/render-titles.sh
+++ tools/render-titles.sh
@@ -9,6 +9,16 @@ toilet -t -f smmono12 --filter border " GAME OVER! " >> gfx/gameover.txt
 toilet -t -f pagga --filter border "      YOU WIN      " > gfx/victory.txt
 toilet -t -f smmono12 --filter border " VICTORY! " >> gfx/victory.txt
 
+for (( LOOP=1; LOOP < 11; LOOP++ )); do
+  if ((LOOP <= 9)); then
+    LOOP_PADDED=$(printf "%02d" ${LOOP})
+  else
+    LOOP_PADDED=${LOOP}
+  fi
+  toilet -t -f pagga --filter border "     LEVEL ${LOOP_PADDED}     " > gfx/level-${LOOP}.txt
+  toilet -t -f smmono12 --filter border "          " >> gfx/level-${LOOP}.txt
+done
+
 for TEXT in gfx/*.txt; do
     ANSI=$(echo ${TEXT} | sed 's/txt/ans/')
     echo "Rendering ${TEXT} to ${ANSI}"
