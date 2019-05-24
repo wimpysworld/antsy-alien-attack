52ef85d65e6b1df18de7d905e0de97153239e0f0
Martin Wimpress <code@flexion.org>
2019-05-24 21:05:47 +0100

    tools: Update render-tools.sh to fully generate title graphics.

--- tools/render-titles.sh
+++ tools/render-titles.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+toilet -t -f pagga --filter border "               ANSI              " > gfx/title.txt
+toilet -t -f smmono12 --filter border " Alien Attack! " >> gfx/title.txt
+
+toilet -t -f pagga --filter border "       YOU LOSE       " > gfx/gameover.txt
+toilet -t -f smmono12 --filter border " GAME OVER! " >> gfx/gameover.txt
+
+toilet -t -f pagga --filter border "      YOU WIN      " > gfx/victory.txt
+toilet -t -f smmono12 --filter border " VICTORY! " >> gfx/victory.txt
+
 for TEXT in gfx/*.txt; do
     ANSI=$(echo ${TEXT} | sed 's/txt/ans/')
     echo "Rendering ${TEXT} to ${ANSI}"
     lolcat -f -F 0.2 "${TEXT}" | sed 's/\[0m/\[30m/g' > "${ANSI}"
+    cat "${ANSI}"
 done
