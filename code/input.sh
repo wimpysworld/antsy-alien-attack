6e4d2d068b80c8e9fc95703ab50a3eb50c68763d
Martin Wimpress <code@flexion.org>
2019-05-25 11:45:07 +0100

    code: Clean up input.sh

--- code/input.sh
+++ code/input.sh
@@ -2,9 +2,9 @@
 
 export KEY=
 export JOYSTICK_THREAD=
-JOY2KEY=$(command -v joy2key)
 
 joystick-setup() {
+  local JOY2KEY=$(command -v joy2key)
   # Is joy2key in the path and is there joystick connected
   if [ -n "${JOY2KEY}" ] && [ -c /dev/input/js0 ]; then
     ${JOY2KEY} -rcfile cfg/xbox360.cfg > /dev/null 2>&1 &
