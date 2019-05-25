307d9eb1bce0533a8da4132d59ddacffd100f31f
Martin Wimpress <code@flexion.org>
2019-05-25 14:07:12 +0100

    code: Cleanup threads.sh

--- code/threads.sh
+++ code/threads.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-threads=()
+export THREADS=()
 
 keep-thread() {
-  local pid=$!
-  threads+=("$pid")
-  echo $pid
+  local PID=$!
+  THREADS+=("${PID}")
+  echo ${PID}
 }
 
 kill-thread() {
-  local pid=$1
-  (kill -KILL "$pid" || true) > /dev/null 2>&1
+  local PID=${1}
+  (kill -KILL "${PID}" || true) > /dev/null 2>&1
 }
 
 terminate-all-threads() {
-  for index in "${!threads[@]}"; do
-    kill-thread "${threads["$index"]}"
-    unset threads["$index"]
+  local LOOP=0
+  for LOOP in "${!THREADS[@]}"; do
+    kill-thread "${THREADS["${LOOP}"]}"
+    unset THREADS["${LOOP}"]
   done
-  threads=()
+  THREADS=()
 }
